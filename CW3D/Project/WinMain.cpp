f8ca130aafab1d0131df616b7028a129096b354a
C0422-Yasui <C0422@oic.jp>
2022-06-29 00:08:49 +0900

    ウィンドウを変更

--- CW3D/Project/WinMain.cpp
+++ CW3D/Project/WinMain.cpp
@@ -1,16 +1,17 @@
 #include	"GameApp.h"
 
+
 int WINAPI _tWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow )
 {
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 
 	Mof::LPFramework pFrame = new Mof::CDX11GameFramework(); 
 	Mof::WINDOWSGAMEFRAMEWORKINITIALIZEINFO Info;
-	Info.pApplication = new CGameApp();
 	Info.WindowCreateInfo.Width = 1920;
 	Info.WindowCreateInfo.Height = 1080;
-
+	Info.WindowCreateInfo.Style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_MAXIMIZE;
 	
+	Info.pApplication = new CGameApp();
 	if ( pFrame->Initialize(&Info) )
 	{
 		pFrame->Run();
